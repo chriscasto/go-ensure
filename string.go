dd262e53733f2dd991a5bacb70b0b797aed48273
Chris Casto <23032560+chriscasto@users.noreply.github.com>
2025-02-19 23:29:27 -0700

    feat: add string regex matching and predefined patterns

--- string.go
+++ string.go
@@ -3,6 +3,30 @@ package ensure
 import (
 	"errors"
 	"fmt"
+	"regexp"
+)
+
+const (
+	// Basic patterns
+	Alpha    = `(?i)^[a-z]+$`
+	AlphaNum = `(?i)^[a-z0-9]+$`
+	Numbers  = `^\d+$`
+	Decimal  = `^\d*\.\d+$`
+
+	// Uuid
+	Uuid4 = `(?i)^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$`
+
+	// Internet
+	Ipv4  = `^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$`
+	Email = `(?i)(?:[a-z0-9!#$%&'*+/=?^_` + "`" +
+		`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_` + "`" +
+		`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])`
+
+	// Hashes
+	Md5    = `^[0-9a-f]{32}$`
+	Sha1   = `^[0-9a-f]{40}$`
+	Sha256 = `^[0-9a-f]{64}$`
+	Sha512 = `^[0-9a-f]{128}$`
 )
 
 // define the max length for string contents in an error message
@@ -98,6 +122,28 @@ func (v *StringValidator) HasLength(l int) *StringValidator {
 					`string "%s" does not have desired length (%d)`,
 					shortenString(str, defaultMaxPrintLength),
 					l),
+			)
+		}
+
+		return nil
+	})
+
+	return v
+}
+
+func (v *StringValidator) Matches(pattern string) *StringValidator {
+	r, err := regexp.Compile(pattern)
+	if err != nil {
+		panic(err)
+	}
+
+	v.tests = append(v.tests, func(str string) error {
+		if !r.MatchString(str) {
+			return errors.New(
+				fmt.Sprintf(
+					`string "%s" does not match expected pattern`,
+					shortenString(str, defaultMaxPrintLength),
+				),
 			)
 		}
 
